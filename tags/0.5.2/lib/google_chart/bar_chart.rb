f4b533161d340ed0e69bc5beb5b904d826c73fa7
deepak.jois <deepak.jois@a5ed0d24-b140-0410-954c-e53ccb4e1efa>
2007-12-14 15:29:02 +0000

    tagging 0.5.2 release

---
+++ tags/0.5.2/lib/google_chart/bar_chart.rb
@@ -0,0 +1,60 @@
+require File.dirname(__FILE__) + '/base'
+module GoogleChart
+    # Generates a Bar Chart. You can specify the alignment(horizontal or vertical) and whether you want the bars to be grouped or stacked
+    # ==== Examples
+    #     bc = GoogleChart::BarChart.new('800x200', "Bar Chart", :vertical, false)
+    #     bc.data "Trend 1", [5,4,3,1,3,5], '0000ff'     
+    class BarChart < Base
+      
+      attr_accessor :alignment, :stacked
+      
+      # Specify the 
+      # * +chart_size+ in WIDTHxHEIGHT format
+      # * +chart_title+ as a string
+      # * +alignment+ as either <tt>:vertical</tt> or <tt>:horizontal</tt>
+      # * +stacked+ should be +true+ if you want the bars to be stacked, false otherwise
+      def initialize(chart_size='300x200', chart_title=nil, alignment=:vertical, stacked=false)
+          super(chart_size, chart_title)
+          @alignment = alignment
+          @stacked = stacked
+          set_chart_type
+          self.show_legend = true
+      end
+      
+      # Set the alignment to either <tt>:vertical</tt> or <tt>:horizontal</tt>
+      def alignment=(value)
+          @alignment = value
+          set_chart_type
+      end
+      
+      # If you want the bar chart to be stacked, set the value to <tt>true</tt>, otherwise set the value to <tt>false</tt> to group it.
+      def stacked=(stacked)
+          @stacked = value
+          set_chart_type
+      end
+
+      def process_data
+          if @data.size > 1              
+                join_encoded_data(@data.collect { |series|
+                  encode_data(series, max_data_value)
+                })
+          else
+              encode_data(@data.flatten,max_data_value)
+          end
+      end
+      
+      private
+      def set_chart_type
+          # Set chart type
+          if alignment == :vertical and stacked == false
+              self.chart_type = :bvg
+          elsif alignment == :vertical and stacked == true
+              self.chart_type = :bvs
+          elsif alignment == :horizontal and stacked == false
+              self.chart_type = :bhg
+          elsif alignment == :horizontal and stacked == true
+              self.chart_type = :bhs
+          end          
+      end
+    end
+end
---
+++ tags/0.5.2/lib/test.rb
@@ -0,0 +1,108 @@
+require 'google_chart'
+
+# Pie Chart
+pc = GoogleChart::PieChart.new('320x200', "Pie Chart",false)
+pc.data "Apples", 40
+pc.data "Banana", 20
+pc.data "Peach", 30
+pc.data "Orange", 60
+puts "\nPie Chart"
+puts pc.to_url
+
+# Pie Chart with no labels
+pc.show_labels = false
+puts "\nPie Chart (with no labels)"
+puts pc.to_url
+
+# Line Chart
+lc = GoogleChart::LineChart.new('320x200', "Line Chart", false)
+lc.data "Trend 1", [5,4,3,1,3,5,6], '0000ff'
+lc.show_legend = true
+lc.data "Trend 2", [1,2,3,4,5,6], '00ff00'
+lc.data "Trend 3", [6,5,4,3,2,1], 'ff0000'
+lc.axis :y, :range => [0,6], :color => 'ff00ff', :font_size => 16, :alignment => :center
+lc.axis :x, :range => [0,6], :color => '00ffff', :font_size => 16, :alignment => :center
+lc.grid :x_step => 100.0/6.0, :y_step => 100.0/6.0, :length_segment => 1, :length_blank => 0
+puts "\nLine Chart"
+puts lc.to_url
+
+# Bar Chart
+bc = GoogleChart::BarChart.new('800x200', "Bar Chart", :vertical, false)
+bc.data "Trend 1", [5,4,3,1,3,5], '0000ff' 
+bc.data "Trend 2", [1,2,3,4,5,6], 'ff0000'
+bc.data "Trend 3", [6,5,4,4,5,6], '00ff00'
+puts "\nBar Chart"
+puts bc.to_url
+
+# Line XY Chart
+lcxy =  GoogleChart::LineChart.new('320x200', "Line XY Chart", true)
+lcxy.data "Trend 1", [[1,1], [2,2], [3,3], [4,4]], '0000ff'
+lcxy.data "Trend 2", [[4,5], [2,2], [1,1], [3,4]], '00ff00'
+puts "\nLine XY Chart"
+puts lcxy.to_url 
+
+# Venn Diagram
+# Supply three vd.data statements of label, size, color for circles A, B, C
+# Then, an :intersections with four values:
+# the first value specifies the area of A intersecting B
+# the second value specifies the area of B intersecting C
+# the third value specifies the area of C intersecting A
+# the fourth value specifies the area of A intersecting B intersecting C
+vd = GoogleChart::VennDiagram.new("320x200", 'Venn Diagram') 
+vd.data "Blue", 100, '0000ff'
+vd.data "Green", 80, '00ff00'
+vd.data "Red",   60, 'ff0000'
+vd.intersections 30,30,30,10
+puts "\nVenn Diagram"
+puts vd.to_url
+
+# Scatter Chart
+sc = GoogleChart::ScatterChart.new('320x200',"Scatter Chart")
+sc.data "Scatter Set", [[1,1,], [2,2], [3,3], [4,4]]
+sc.max_value [5,5] # Setting the max value
+sc.axis :x, :range => [0,5]
+sc.axis :y, :range => [0,5], :labels => [0,1,2,3,4,5]
+sc.point_sizes [10,15,30,55] # Optional
+puts "\nScatter Chart"
+puts sc.to_url
+
+# Solid fill
+lc.fill(:background, :solid, {:color => 'fff2cc'})
+lc.fill(:chart, :solid, {:color => 'ffcccc'})
+puts "\nLine Chart with Solid Fill"
+puts lc.to_url
+
+# Gradient fill
+lc.fill :background, :gradient, :angle => 0,  :color => [['76A4FB',1],['ffffff',0]]
+lc.fill :chart, :gradient, :angle => 0, :color => [['76A4FB',1],
+                                                   ['ffffff',0]]
+puts "\nLine Chart with Gradient Fill"
+puts lc.to_url
+
+# Stripes Fill
+lc.fill :chart, :stripes, :angle => 90, :color => [['76A4FB',0.2],
+                                                   ['ffffff',0.2]]
+puts "\nLine Chart with Stripes Fill"
+puts lc.to_url
+
+# Adding Extra params
+lc = GoogleChart::LineChart.new('320x200', "Line Chart", false)
+lc.title_color = 'ff00ff'
+lc.data "Trend 1", [5,4,3,1,3,5,6], '0000ff'
+lc.data "Trend 2", [1,2,3,4,5,6], '00ff00'
+lc.data "Trend 3", [6,5,4,3,2,1], 'ff0000'
+lc.max_value 10 # Setting max value for simple line chart 
+lc.range_marker :horizontal, :color => 'E5ECF9', :start_point => 0.1, :end_point => 0.5
+lc.range_marker :vertical, :color => 'a0bae9', :start_point => 0.1, :end_point => 0.5
+puts "\nLine Chart with range markers"
+puts lc.to_url
+
+# Bryan Error condition
+lcxy =  GoogleChart::LineChart.new('320x200', "Line XY Chart", true)
+lcxy.data 'A', [[0, 32], [1, 15], [2, 23], [3, 18], [4, 41],  [5, 53]],'0000ff'
+lcxy.data 'B', [[0, 73], [1, 0],  [2, 28], [3, 0],  [4, 333], [5, 0]], '00ff00'
+lcxy.data 'C', [[0, 22], [1, 26], [2, 14], [3, 33], [4, 17],  [5, 7]], 'ff0000'
+lcxy.data 'D', [[0, 4],  [1, 39], [2, 0],  [3, 5],  [4, 11],  [5, 14]], 'cc00ff'
+puts "\nBryan Error Condition"
+puts lcxy.to_url
+
