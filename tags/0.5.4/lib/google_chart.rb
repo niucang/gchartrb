2745d62118507176169a61cd1c87e44bb8373c2c
deepak.jois <deepak.jois@a5ed0d24-b140-0410-954c-e53ccb4e1efa>
2008-01-07 12:07:18 +0000

    Tagging a new release

---
+++ tags/0.5.4/lib/google_chart.rb
@@ -0,0 +1,10 @@
+%w(
+    base
+    pie_chart
+    line_chart
+    bar_chart
+    venn_diagram
+    scatter_chart    
+).each do |filename|
+    require File.dirname(__FILE__) + "/google_chart/#{filename}"
+end
